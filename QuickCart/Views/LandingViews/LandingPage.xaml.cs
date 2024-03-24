a1eef82f8fbfb18218bbb938ed1842538cee8362
Matthew Bennett <90990547+MattRBennett@users.noreply.github.com>
2024-03-24 17:54:25 +0200

    landing page

--- QuickCart/Views/LandingViews/LandingPage.xaml.cs
+++ QuickCart/Views/LandingViews/LandingPage.xaml.cs
@@ -1,3 +1,7 @@
+using System.Drawing;
+using System.Resources;
+using System.Collections.Generic;
+
 namespace QuickCart.Views.LandingViews;
 
 public partial class LandingPage : ContentPage
@@ -5,5 +9,14 @@ public partial class LandingPage : ContentPage
 	public LandingPage()
 	{
 		InitializeComponent();
+
 	}
+
+    //protected override void OnAppearing()
+    //{
+    //    base.OnAppearing();
+
+    //    LandingPageImage.Source = "home_active.png";
+
+    //}
 }
